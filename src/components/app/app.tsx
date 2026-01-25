188d9254d1adb3435acb7c57b67f5a65b9f7c8b5
Valentin Braun <braunvlntn@yandex.ru>
2026-01-25 23:53:59 +0500

    настроил роуты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,92 @@
-import { ConstructorPage, NotFound404 } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './layout';
+import { OrderInfo } from '../order-info';
+import { Modal } from '../modal';
+import { IngredientDetails } from '../ingredient-details';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    errorElement: <NotFound404 />,
     children: [
       {
         path: '/',
         element: <ConstructorPage />
+      },
+      {
+        path: '/feed',
+        element: <Feed />,
+        children: [
+          {
+            path: '/feed:number',
+            element: (
+              <Modal title='Информация о заказе' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            )
+          }
+        ]
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+      {
+        path: '/forgot-password',
+        element: <ForgotPassword />
+      },
+      {
+        path: '/reset-password',
+        element: <ResetPassword />
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+        children: [
+          {
+            path: '/profile/orders',
+            element: <ProfileOrders />,
+            children: [
+              {
+                path: '/profile/orders/:number',
+                element: (
+                  <Modal title='Информация о заказе' onClose={() => {}}>
+                    <OrderInfo />
+                  </Modal>
+                )
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: '/ingredients/:id',
+        element: (
+          <Modal title='Ингредиенты' onClose={() => {}}>
+            <IngredientDetails />
+          </Modal>
+        )
+      },
+      {
+        path: '*',
+        element: <NotFound404 />
       }
     ]
   }
