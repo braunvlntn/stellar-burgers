9f7ebc388e108dc6c4c19ec8423dcbd778ee92ce
Valentin Braun <braunvlntn@yandex.ru>
2026-01-31 01:33:46 +0500

    получил ингредиенты на странице ингредиента

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,18 +1,32 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
 import { useParams } from 'react-router-dom';
-import { useSelector } from '../../services/store';
-import { selectIngredients } from '../../services/ingredients/selectors';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  selectIngredients,
+  selectLoading
+} from '../../services/ingredients/selectors';
+import { fetchIngredients } from '../../services/ingredients/thunks';
 
 export const IngredientDetails: FC = () => {
+  const dispatch = useDispatch();
   let { id } = useParams();
+
+  const ingredients = useSelector(selectIngredients);
+  const ingredientsLoading = useSelector(selectLoading);
+
+  useEffect(() => {
+    if (!ingredients.length && !ingredientsLoading) {
+      dispatch(fetchIngredients());
+    }
+  }, [ingredients, ingredientsLoading]);
 
   const ingredientData = useSelector(selectIngredients).find(
     (ingredient) => ingredient._id === id
   );
 
-  if (!ingredientData) {
+  if (!ingredientData || ingredientsLoading) {
     return <Preloader />;
   }
 
